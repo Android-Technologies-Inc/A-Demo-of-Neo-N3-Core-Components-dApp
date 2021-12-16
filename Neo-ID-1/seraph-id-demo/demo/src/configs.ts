752e065d9607990ffd6417d3232c5e5a43f32e61
Sam Wang <leiw5173@gmail.com>
2021-12-16 14:48:47 +0800

    Add smart contracts

---
+++ Neo-ID-1/seraph-id-demo/demo/src/configs.ts
@@ -0,0 +1,29 @@
+// Copyright (c) 2019 Swisscom Blockchain AG
+// Licensed under MIT License
+
+import { DIDNetwork } from '@sbc/seraph-id-sdk'
+
+export const NEO_RPC_URL = 'http://seed1t.neo.org:20332';
+export const DID_NETWORK = DIDNetwork.TestNet;
+
+export const GOVERNMENT_SCRIPT_HASH = '0xcdec7e3c578f6d9e5f0fc48368b69f5e3759c635';
+export const AGENCY_SCRIPT_HASH = '0xcdec7e3c578f6d9e5f0fc48368b69f5e3759c635';
+
+export const GOVERNMENT_ISSUER_PRIVATE_KEY = 'KwgorFpHTbfwCko4ZJ5dZ7Xt1heCJHsscKy3m36Du6bVrbpuSAup';
+export const AGENCY_ISSUER_PRIVATE_KEY = 'KwgorFpHTbfwCko4ZJ5dZ7Xt1heCJHsscKy3m36Du6bVrbpuSAup';
+export const FLAT_WALLET_ADDRESS = '0x5a4e405ed7fb167569199c46758105ecedd654f7';  
+
+export const PRICE_FEED_SERVICE = '0x89d9839aa840a0bc55b64501faeac3ab037f471d';
+export const OKEX_PROVIDER = '0x9a2a4fe4aef9453edb5b735792c5aac99bfedeb0';
+
+export const CONTAINER_ID = 'AssBPkHiryqgSf5j96hje6rS2QJ8PMEkJx34iCXZWbsP';
+export const OBJECT_ID1 = 'CWG5gTFYyAeFUQwj8XAHkMttk9z9fbn5sBJqN97LAvVF';
+export const OBJECT_ID2 = '9FYG3a93v4tycaCc7w4QnnbM1EyDndNTGFB3QHoBs1oT';
+export const OBJECT_ID3 = 'F9Z8NNx76drdzqufXc136dV8q1qxpXKkJvVNj3P1a72p';
+export const OBJECT_ID4 = 'EFT77HCB8xMXCUyaDPBVvakJbEcu6SZXNcHNFbWogm5g';
+export const OBJECT_ID5 = '6QGyCDfAV99fDMbMbc8Mv9EiNy57SrKHTE314LS5Ymmw';
+export const OBJECT_ID6 = 'AaszehhYEUrc7ePGiGD8kYXjo3b6i8mJb8ZDsAactWL2';
+
+export const PASSPORT_SCHEMA_NAME = 'Passport1';
+export const ACCESS_KEY_SCHEMA_NAME = 'AccessKey1';
+export const MAGIC = 844378958;
